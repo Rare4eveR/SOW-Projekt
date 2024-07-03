df83d8d9e5d9b303e57f22ec1b85353ef7ef04b5
Arek <arkadius.skatulla@edu.bib.de>
2024-07-03 11:37:39 +0200

    Kleine überarbeitung in Angestellte

--- ProjektCode/SWO Projekt/src/Manager.java
+++ ProjektCode/SWO Projekt/src/Manager.java
@@ -1,2 +1,85 @@
-public class Manager {
+import java.time.LocalDate;
+
+public class Manager extends Mitarbeiter {
+    private double managerLohn;
+    private double provisionsSatz;
+    private double gewinn;
+    private String laufNummer;
+
+    public Manager(String nachname, String vorname, String adresse, LocalDate geburtsdatum, LocalDate eintrittsdatum, char geschlecht, double managerLohn, double provisionsSatz) throws Exception {
+        super(nachname, vorname, adresse, geburtsdatum, eintrittsdatum, geschlecht, null, null);
+        try {
+            setManagerLohn(managerLohn);
+            setProvisionsSatz(provisionsSatz);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        laufNummer = super.generateLaufnummer();
+    }
+
+    @Override
+    public String generateLaufnummer() {
+        return laufNummer;
+    }
+
+    public double getManagerLohn() {
+        return managerLohn;
+    }
+
+    public void setManagerLohn(double managerLohn) throws Exception {
+        if (managerLohn <= 0) {
+            throw new Exception("Der Managerlohn muss größer als 0 sein.");
+        }
+        this.managerLohn = managerLohn;
+    }
+
+    public double getProvisionsSatz() {
+        return provisionsSatz;
+    }
+
+    public void setProvisionsSatz(double provisionsSatz) throws Exception {
+        if (provisionsSatz <= 0) {
+            throw new Exception("Der Provisionssatz muss größer als 0 sein.");
+        }
+        this.provisionsSatz = provisionsSatz;
+    }
+
+    public double berechneProvision() {
+        if (gewinn <= 0) {
+            return 0;
+        }
+        double provision = getGewinn();
+        setGewinn(0);
+        return provision * provisionsSatz;
+    }
+
+    public void lohnErhoehen(double prozentsatz) throws Exception {
+        if (prozentsatz > 0 && prozentsatz <= 10) {
+            managerLohn += managerLohn * (prozentsatz / 100);
+        } else {
+            throw new Exception("Der Prozentsatz muss größer als 0 und kleiner als 10 sein.");
+        }
+    }
+
+    public double getGewinn() {
+        return gewinn;
+    }
+
+    public void setGewinn(double gewinn) {
+        this.gewinn = gewinn;
+    }
+
+    @Override
+    public double GehaltBerechnen() {
+        return managerLohn + berechneProvision();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                "Managerlohn=" + managerLohn +
+                "€, Provisionssatz=" + provisionsSatz +
+                "%, Gewinn=" + gewinn +
+                "€, Laufnummer='" + laufNummer + '\'';
+    }
 }
