65b12ef943c3acd4d3af501fe64f2b7e0b28017b
Arek <arkadius.skatulla@edu.bib.de>
2024-07-03 10:33:21 +0200

    Mitarbeiter Klasse Aktualisiert

--- ProjektCode/SWO Projekt/src/Angestellte.java
+++ ProjektCode/SWO Projekt/src/Angestellte.java
@@ -1,61 +1,80 @@
 import java.time.LocalDate;
-
 public class Angestellte extends Mitarbeiter{
+    private double stundenlohn;
+    private double ueberstundenlohn;
+    private double stundenanzahl;
+    private double ueberstundenanzahl;
 
-    public Angestellte(String _nachname, String _vorname, String _adresse, LocalDate _geburtsdatum, LocalDate _eintrittsdatum, double stundenLohn, double ueberstundenLohn,
-                       double stundenAnzahl, double ueberStundenAnzahl) {
-        super(_nachname, _vorname, _adresse, _geburtsdatum, _eintrittsdatum);
-        StundenLohn = stundenLohn;
-        UeberstundenLohn = ueberstundenLohn;
-        StundenAnzahl = stundenAnzahl;
-        this.UeberStundenAnzahl = ueberStundenAnzahl;
+    public Angestellte(char geschlecht, String vorname, String nachname, String adresse, LocalDate geburtsdatum,
+                       LocalDate eintrittsdatum, double stundenlohn, double ueberstundenlohn, double stundenanzahl,
+                       double ueberstundenanzahl) throws Exception {
+        super(vorname, nachname, adresse, geburtsdatum, eintrittsdatum, geschlecht);
+        try{
+            setStundenlohn(stundenlohn);
+            setUeberstundenlohn(ueberstundenlohn);
+            setStundenanzahl(stundenanzahl);
+            setUeberstundenanzahl(ueberstundenanzahl);
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 
-    private double StundenLohn;
-	private double UeberstundenLohn;
-	private double StundenAnzahl;
-    private double UeberStundenAnzahl;
-
-	public void Angestellte(double StundenLohn, double StundenAnzahl, double UeberstundenLohn, double ueberStundenAnzahl ){
-
-
-    }
-	public double GehaltBerechnen(){
-
-        double Gehalt = StundenLohn *  StundenAnzahl + UeberstundenLohn * UeberStundenAnzahl;
-
-        return Gehalt;
+    public Angestellte(String nachname, String vorname, String adresse, LocalDate geburtsdatum, LocalDate eintrittsdatum, char geschlecht) throws Exception {
+        super(nachname, vorname, adresse, geburtsdatum, eintrittsdatum, geschlecht);
     }
 
-    public double getStundenLohn() {
-        return StundenLohn;
+    public double getStundenlohn() {
+        return stundenlohn;
     }
 
-    public void setStundenLohn(double stundenLohn) {
-        StundenLohn = stundenLohn;
+    public void setStundenlohn(double stundenlohn) throws Exception {
+        if (stundenlohn <= 0){
+            throw new Exception("Stundenlohn should be greater than 0");
+        }
+        this.stundenlohn = stundenlohn;
     }
 
-    public double getUeberstundenLohn() {
-        return UeberstundenLohn;
+    public double getUeberstundenlohn() {
+        return ueberstundenlohn;
     }
 
-    public void setUeberstundenLohn(double ueberstundenLohn) {
-        UeberstundenLohn = ueberstundenLohn;
+    public void setUeberstundenlohn(double ueberstundenlohn) throws Exception {
+        if (ueberstundenlohn <= 0){
+            throw new Exception("Ueberstundenlohn should be greater than 0");
+        }
+        this.ueberstundenlohn = ueberstundenlohn;
     }
 
-    public double getStundenAnzahl() {
-        return StundenAnzahl;
+    public double getStundenanzahl() {
+        return stundenanzahl;
     }
 
-    public void setStundenAnzahl(double stundenAnzahl) {
-        StundenAnzahl = stundenAnzahl;
+    public void setStundenanzahl(double stundenanzahl) throws Exception {
+        if (stundenanzahl <= 0){
+            throw new Exception("Stundenanzahl should be greater than 0");
+        }
+        this.stundenanzahl = stundenanzahl;
     }
 
-    public double getUeberStundenAnzahl() {
-        return ueberStundenAnzahl;
+    public double getUeberstundenanzahl() {
+        return ueberstundenanzahl;
     }
 
-    public void setUeberStundenAnzahl(double ueberStundenAnzahl) {
-        this.ueberStundenAnzahl = ueberStundenAnzahl;
+    public void setUeberstundenanzahl(double ueberstundenanzahl) throws Exception {
+        if (ueberstundenanzahl < 0){
+            throw new Exception("Ueberstundenanzahl cannot be negative");
+        }
+        this.ueberstundenanzahl = ueberstundenanzahl;
+    }
+
+    public void LohnErhoehen() {
+        stundenlohn += stundenlohn * (5 / 100);
+        ueberstundenlohn += ueberstundenlohn * (5 / 100);
+    }
+
+    @Override
+    public double GehaltBerechnen() {
+        return 0;
     }
 }
