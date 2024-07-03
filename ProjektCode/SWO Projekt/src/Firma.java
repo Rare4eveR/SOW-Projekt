71acde2050a09bcb6e22939520cd3857855a69ac
Arek <arkadius.skatulla@edu.bib.de>
2024-07-03 13:42:50 +0200

    Mitarbeiter Hinzufügen bzw Entfernen Methode und die Min Max lohn Methoden

--- ProjektCode/SWO Projekt/src/Firma.java
+++ ProjektCode/SWO Projekt/src/Firma.java
@@ -2,7 +2,7 @@ public class Firma {
 
 
     private Mitarbeiter[] angestellteMitarbeiter = new Mitarbeiter[20];
-    private Mitarbeiter[] ehemaligeMitarbeiter = ehemaligeMitarbeiter;
+    private Mitarbeiter[] ehemaligeMitarbeiter;
     private static int anzahlMitarbeiter = 0;
 
     public Firma(Mitarbeiter[] angestellteMitarbeiter) {
@@ -11,6 +11,10 @@ public class Firma {
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
+    }
+
+    private void angestellteMitarbeiter(Mitarbeiter[] angestellteMitarbeiter) {
+
     }
 
     public Mitarbeiter[] getAngestellteMitarbeiter() {
@@ -49,6 +53,64 @@ public class Firma {
         }
         Firma.anzahlMitarbeiter = anzahlMitarbeiter;
     }
+
+    public void mitarbeiterHinzufuegen(Mitarbeiter mitarbeiter) throws Exception {
+        if (mitarbeiter == null) {
+            throw new Exception("mitarbeiter must not be null");
+        }
+        for (int i = 0; i < getAngestellteMitarbeiter().length; i++) {
+            if (getAngestellteMitarbeiter()[i] == null) {
+                getAngestellteMitarbeiter()[i] = mitarbeiter;
+            }
+        }
+    }
+
+    public void mitarbieterEntfernen(Mitarbeiter mitarbeiter) throws Exception {
+        if (mitarbeiter == null) {
+            throw new Exception("mitarbeiter must not be null");
+        }
+        for (int i = 0; i < getAngestellteMitarbeiter().length; i++) {
+            if (getAngestellteMitarbeiter()[i] == mitarbeiter) {
+                for (int j = i + 1; j < getEhemaligeMitarbeiter().length; j++) {
+                    if (getEhemaligeMitarbeiter()[j] != null) {
+                        getEhemaligeMitarbeiter()[j] = mitarbeiter;
+                    }
+                }
+                getAngestellteMitarbeiter()[i] = null;
+            }
+        }
+    }
+
+    public double MinLohn(){
+        double temp = MaxLohn();
+        for (int i = 0; i < angestellteMitarbeiter.length; i++) {
+            if (angestellteMitarbeiter[i] != null) {
+                if (angestellteMitarbeiter[i].GehaltBerechnen() < temp){
+                    temp = angestellteMitarbeiter[i].GehaltBerechnen();
+                }
+            } else if (angestellteMitarbeiter[i] == null) {
+                System.out.println("The List is empty at index " + i);
+                break;
+            }
+        }
+        return temp;
+    }
+
+    public double MaxLohn(){
+        double temp = 0.0;
+        for (int i = 0; i < angestellteMitarbeiter.length; i++) {
+            if (angestellteMitarbeiter[i] != null) {
+                if (angestellteMitarbeiter[i].GehaltBerechnen() > temp){
+                    temp = angestellteMitarbeiter[i].GehaltBerechnen();
+                }
+            } else if (angestellteMitarbeiter[i] == null) {
+                System.out.println("The List is empty at index " + i);
+                break;
+            }
+        }
+        return temp;
+    }
+
 
 }
 
